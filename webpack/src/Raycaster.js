61d215d08107ff3ab2e814c7a4d27e38bdaa7c41
张博文 <zhangbowen@haomo.ai>
2022-10-26 19:09:43 +0800

    update

--- webpack/src/Raycaster.js
+++ webpack/src/Raycaster.js
@@ -9,86 +9,75 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 console.log("[Bowen] ~ file: Raycaster.js ~ line 10 ~ scene", scene)
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000)
-camera.position.set(0,0,500)
+camera.position.set(0, 0, 1000)
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // 创建图形代码 ================================================================
-
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const geometry = new THREE.BoxGeometry( 100,100,100);
-// const cube = new THREE.Mesh(geometry, material)
-// scene.add(cube)
-for (let x = 0; x < 10; x++) {
-    const group = new THREE.Group()
-    for (let y = 0; y < 10; y++) {
-        const geometry = new THREE.BoxGeometry(5,5,5);
+// 做 25 个小方块
+for (let x = 0; x < 5; x++) {
+    for (let y = 0; y < 5; y++) {
+        const geometry = new THREE.BoxGeometry(100, 100, 100);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         const cube = new THREE.Mesh(geometry, material)
-        let px= x *10 + 15
-        let py= y *10 + 15
-        // if(x<5) px = 0 -px
-        console.log("[Bowen] ~ file: Raycaster.js ~ line 33 ~ px", px)
-        // if(y<5) py = 0 -py
-        console.log("[Bowen] ~ file: Raycaster.js ~ line 35 ~ py", py)
-        cube.position.set(px,py, 10)
+        // 根据立方体创建边缘
+        const edges = new THREE.EdgesGeometry(geometry);
+        // 根据边缘创建线
+        const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
+        // 将边缘线加进立方体
+        cube.add(line)
+        // 定位 每个顶点间距 200 （2 倍宽度）
+        let px = (x -2) * 200 
+        let py = (y -2) * 200 
+        cube.position.set(px, py, -500)
         scene.add(cube)
-  
+
     }
-    // // 放入场景
-    // scene.add( group );
 }
 
 
-
+// 光线投射
 const raycaster = new THREE.Raycaster();
+// 二维向量
 const pointer = new THREE.Vector2();
-
-function onPointerMove( event ) {
-// console.log("[Bowen] ~ file: Raycaster.js ~ line 35 ~ onPointerMove ~ event", event)
-
-	// 将鼠标位置归一化为设备坐标。x 和 y 方向的取值范围是 (-1 to +1)
-
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+// 添加 pointermove 事件监听 改变 pointer 坐标
+function onPointerMove(event) {
+    // console.log("[Bowen] ~ file: Raycaster.js ~ line 35 ~ onPointerMove ~ event", event)
+    // 将鼠标位置归一化为设备坐标。x 和 y 方向的取值范围是 (-1 to +1)
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
 }
-window.addEventListener( 'pointermove', onPointerMove );
+window.addEventListener('pointermove', onPointerMove);
 // 渲染代码 =====================================================================
 
 function render() {
-   // 通过摄像机和鼠标位置更新射线
-	raycaster.setFromCamera( pointer, camera );
-
-	// 计算物体和射线的焦点
-	const intersects = raycaster.intersectObjects( scene.children );
-	console.log("[Bowen] ~ file: Raycaster.js ~ line 52 ~ render ~ intersects", intersects)
-
-	for ( let i = 0; i < intersects.length; i ++ ) {
-
-		intersects[ i ].object.material.color.set( 0xff0000 );
-
-	}
-
-	renderer.render( scene, camera );
-
+    // 通过摄像机和鼠标位置更新射线
+    raycaster.setFromCamera(pointer, camera);
+    // 计算物体和射线的焦点 将相交的物体数据返回
+    const intersects = raycaster.intersectObjects(scene.children);
+    console.log("[Bowen] ~ file: Raycaster.js ~ line 52 ~ render ~ intersects", intersects)
+    // 遍历每个物体设置颜色
+    for (let i = 0; i < intersects.length; i++) {
+        intersects[i].object.material.color.set(0xff0000);
+    }
+    renderer.render(scene, camera);
 }
 // 调用
-
 
 function animate() {
     requestAnimationFrame(animate);
     render()
     for (const cube of scene.children) {
-        //   cube.rotation.x += 0.01;
-        // cube.rotation.y += 0.01;
-    }
-   
     /*
     2.1 使立方体动起来 
      */
     // cube.rotation.x += 0.01;
     // cube.rotation.y += 0.01;
+    }
+
+  
 }
 // 调用
 animate();
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,4 +3,7 @@
 // import "./TextureLoader"    // 纹理加载器
 // import "./SpriteMaterial" // 精灵图材质
 // import "./Line" // 线
-import "./Raycaster" // 光线投射
+// import "./Raycaster" // 光线投射
+// import "./OrbitControls" // 轨道控制
+// import "./AxesHelper" // 模拟3个坐标轴的对象
+import "./pos_scale_rote" // 定位 缩放  旋转
